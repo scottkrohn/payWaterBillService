c0d17b563c99228ca516348dcb0163e61b920f95
Scott Krohn <skrohn86@gmail.com>
2015-10-21 12:03:24 -0700

    First commit

---
+++ PayWaterBill/User.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace PayWaterBill
+{
+	public class AllUsers
+	{
+		private List<User> users;
+
+		public AllUsers()
+		{
+			users = new List<User>();	
+		}
+
+		public void saveToFile(string filename)
+		{
+			StreamWriter writer = new StreamWriter(filename);
+			for(int currentUser = 0; currentUser != users.Count; currentUser++)
+			{
+				writer.WriteLine("[USER]");
+				writer.WriteLine(users[currentUser].getName());
+				List<double> payments = users[currentUser].getPayments();
+				for(int currentPayment = 0; currentPayment != payments.Count; currentPayment++)
+				{
+					writer.WriteLine(payments[currentPayment].ToString());
+				}
+			}
+			writer.Close();
+		}
+
+		// Add a payment amount for a user.
+		public bool makePayment(string name, double amount)
+		{
+			for (int i = 0; i != users.Count; i++)
+			{ 
+				if(users[i].getName() == name)
+				{
+					users[i].addPayment(amount);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		// Return a List of payment amounts for a user.
+		public List<double> getUsersPayments(string name)
+		{
+			for(int i = 0; i != users.Count; i++)
+			{
+				if(users[i].getName() == name)
+				{
+					return users[i].getPayments();
+				}
+			}
+			// If the user wasn't found, return null
+			return null;
+		}
+
+		// Add a new user to the list of all users given a name.
+		public bool addUser(string name)
+		{
+			if(!userExists(name))
+			{
+				users.Add(new User(name));
+				return true;
+			}
+			return false;
+		}
+
+		// Add a new user to the list of all users given a User object.
+		public bool addUser(User u)
+		{
+			if(!userExists(u.getName()))
+			{
+				users.Add(u);
+				return true;
+			}
+			return false;
+		}
+
+		// Check if a user exists in the list of all users.
+		public bool userExists(string name)
+		{
+			for(int i = 0; i != users.Count; i++)
+			{
+				if(users[i].getName() == name)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	public class User
+	{
+		private string name;
+		private List<double> payments;
+
+		public User(string name)
+		{
+			this.name = name;
+			payments = new List<double>();
+		}
+
+		public void addPayment(double amount)
+		{
+			payments.Add(amount);
+		}
+
+		public List<double> getPayments()
+		{
+			return payments;
+		}
+
+		public string getName()
+		{
+			return name;
+		}
+	}
+}
---
+++ PayWaterBill/payWaterBillService.svc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using System.Web;
+
+namespace PayWaterBill
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+		static string location = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data");
+		static string USER_INFO_FILENAME = Path.Combine(location, "UserData.txt");
+
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		// Make a payment for a user.
+		public bool makePayment(string name, double amount)
+		{
+			AllUsers users = loadUserInfo(USER_INFO_FILENAME);
+			bool success = users.makePayment(name, amount);
+			users.saveToFile(USER_INFO_FILENAME);
+			return success;
+		}
+
+		// Return a list of all previous payments made for a given user.
+		public double[] getPreviousPayments(string name)
+		{
+			AllUsers users = loadUserInfo(USER_INFO_FILENAME);
+			double[] payments = users.getUsersPayments(name).ToArray();
+			if(payments != null)
+			{
+				return payments;
+			}
+			throw new Exception(string.Format("User: {0} not found.", name));
+		}
+
+		// Add a new user to the list of all users.
+		public bool addNewUser(string name)
+		{
+			AllUsers users = loadUserInfo(USER_INFO_FILENAME);
+			bool success = users.addUser(name);
+			users.saveToFile(USER_INFO_FILENAME);
+			return success;
+		}
+
+		// Return true if a user already is registered.
+		public bool userExists(string name)
+		{
+			AllUsers users = loadUserInfo(USER_INFO_FILENAME);
+			if(name == "")
+			{
+				return false;
+			}
+			return users.userExists(name);
+		}
+
+		// Load user info from the textfile on the server. Parses the text file at USER_INFO_FILENAME
+		private AllUsers loadUserInfo(string filename)
+		{
+			AllUsers users = new AllUsers();
+			StreamReader reader = new StreamReader(filename);
+			while(!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();	// reads [USER]
+				line = reader.ReadLine();			// reads the user's name
+				User currentUser = new User(line);	// Create User object for the current user being parsed.
+				// If [ is found immediately follow a user's name, then the user hasn't made any payments.
+				if (reader.Peek() == '[')
+				{
+					users.addUser(currentUser);
+					continue;	
+				}
+				else
+				{
+					// Get all the payments made by a user and add them to User object.
+					while(reader.Peek() != '[' && !reader.EndOfStream)
+					{
+						line = reader.ReadLine();
+						currentUser.addPayment(Convert.ToDouble(line));
+					}
+				}
+				users.addUser(currentUser);	// add User object to the list of all users.
+			}
+			reader.Close();
+			return users;
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
